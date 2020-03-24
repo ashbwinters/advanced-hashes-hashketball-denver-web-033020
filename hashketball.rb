dd16a3093c03b8fb46f05c6afaf30c78f4469646
Ashley Winters <ashbwinters@gmail.com>
2020-03-24 01:17:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ def player_numbers(team)
   game_hash.each do |home_or_away, key|
     if key[:team_name] == team
       key[:players].each do |jersey_number|
-        team_numbers << jersey_number[:number]
+        team_numbers << [:number]
       end
     end
   end
